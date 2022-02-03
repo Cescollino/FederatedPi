f564ca9c7a01889078fcd039e702061a541a29ec
Cescollino <67806795+Cescollino@users.noreply.github.com>
2022-02-03 09:39:44 -0500

    unfinished training script

---
+++ Client/training.py
@@ -0,0 +1,43 @@
+# this file will be used to train and
+# report accuracy results of the model
+
+import os
+import copy
+import time
+import pickle
+
+import numpy as np #numbers
+from tqdm import tqdm #progress bar
+import torch #pytorch
+from tensorboardX import SummaryWriter #display metrics
+from options import args_parser #parsing arguments when calling py from terminal
+from update import LocalUpdate, test_inference
+
+from options import args_parser
+from update import LocalUpdate, test_inference
+from utils import get_dataset, average_weights, exp_details
+
+from torch import nnclass Network(nn.Module):
+
+    def __init__(self):
+        super().__init__()
+
+        # Inputs to hidden layer linear transformation
+        self.hidden = nn.Linear(3, 5)
+        # Output layer, 10 units - one for each digit
+        self.output = nn.Linear(5, 2)
+        # Define sigmoid activation and softmax output
+        self.sigmoid = nn.Sigmoid()
+        self.softmax = nn.Softmax(dim=1)
+
+    def forward(self, x):
+        # Pass the input tensor through each of our operations
+        x = self.hidden(x)
+        x = self.sigmoid(x)
+        x = self.output(x)
+        x = self.softmax(x)
+
+        return x
+
+model = Network()
+model
