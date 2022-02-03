17ef58fec0d112dfc3909a1eabd36c02aee2d593
Cescollino <67806795+Cescollino@users.noreply.github.com>
2022-02-03 09:38:27 -0500

    small changes

--- Client/datagenerator.py
+++ Client/datagenerator.py
@@ -2,9 +2,11 @@
 #Temperature values range from 20C (death) to 43C (highest recorded fever) in humans, 36.5 to 37.5 being the o.k. range
 #Resting heart rate values range in humans range from 60 (low for an old person) and 190 (high for a newborn)
 
-    #Nouveau-né : 140 ± 50
+    #newborn : 140 ± 50
     #1–2 ans : 110 ± 40
     #3–5 ans : 105 ± 35
     #6–12 ans : 95 ± 30
     #adolescent ou adulte : 70 ± 10
     #personne âgée : 65 ± 5
+
+#We will generate values based on the age of subjects and a gaussian normal distribution
