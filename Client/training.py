04c936b255f1b8ab0fe9db1c9b7778e9f69b6722
khezemi-nour <khezemin7@gmail.com>
2022-02-23 16:09:27 -0500

    training and test

--- Client/training.py
+++ Client/training.py
@@ -10,13 +10,11 @@ import numpy as np #numbers
 from tqdm import tqdm #progress bar
 import torch #pytorch
 from tensorboardX import SummaryWriter #display metrics
-from options import args_parser #parsing arguments when calling py from terminal
-from update import LocalUpdate, test_inference
+import syft as sy
 
-from options import args_parser
-from update import LocalUpdate, test_inference
-from utils import get_dataset, average_weights, exp_details
-
+import torch.nn.functional as F
+import torch.optim as optim
+from torchvision import datasets, transforms
 from torch import nn
 class Network(nn.Module):
 
@@ -41,4 +39,95 @@ class Network(nn.Module):
         return x
 
 model = Network()
-model
+hook = sy.TorchHook(torch) # add extra functionality to PyTorch
+# create a machine owned by worker1
+worker1 = sy.VirtualWorker(hook=hook,id='worker1')
+# create a machine owned by worker2
+worker2 = sy.VirtualWorker(hook=hook,id='worker2')
+
+### learning task parameters
+class Arguments():
+    def __init__(self):
+        self.batch_size = 64
+        self.test_batch_size = 1000
+        self.epochs = 10
+        self.lr = 0.01
+        self.momentum = 0.5
+        self.no_cuda = False
+        self.seed = 1
+        self.log_interval = 10
+        self.save_model = False
+
+args = Arguments()
+
+use_cuda = not args.no_cuda and torch.cuda.is_available()
+
+torch.manual_seed(args.seed)
+
+device = torch.device("cuda" if use_cuda else "cpu")
+
+kwargs = {'num_workers': 1, 'pin_memory': True} if use_cuda else {}
+####### data loading and sending to workers
+federated_train_loader = sy.FederatedDataLoader( # <-- this is now a FederatedDataLoader 
+    datasets.MNIST('../data.csv', train=True, download=True,
+                   transform=transforms.Compose([
+                       transforms.ToTensor(),
+                       transforms.Normalize((0.1307,), (0.3081,))
+                   ]))
+    .federate((worker1, worker2)), # <-- NEW: we distribute the dataset across all the workers, it's now a FederatedDataset
+    batch_size=args.batch_size, shuffle=True, **kwargs)
+
+test_loader = torch.utils.data.DataLoader(
+    datasets.MNIST('../data.csv', train=False, transform=transforms.Compose([
+                       transforms.ToTensor(),
+                       transforms.Normalize((0.1307,), (0.3081,))
+                   ])),
+    batch_size=args.test_batch_size, shuffle=True, **kwargs)
+
+#### define the train and test function
+def train(args, model, device, train_loader, optimizer, epoch):
+    model.train()
+    for batch_idx, (data, target) in enumerate(federated_train_loader): # <-- now it is a distributed dataset
+        model.send(data.location) # <-- NEW: send the model to the right location
+        data, target = data.to(device), target.to(device)
+        optimizer.zero_grad()
+        output = model(data)
+        loss = F.nll_loss(output, target)
+        loss.backward()
+        optimizer.step()
+        model.get() # <-- NEW: get the model back
+        if batch_idx % args.log_interval == 0:
+            loss = loss.get() # <-- NEW: get the loss back
+            print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
+                epoch, batch_idx * args.batch_size, len(train_loader) * args.batch_size, #batch_idx * len(data), len(train_loader.dataset),
+                100. * batch_idx / len(train_loader), loss.item()))
+
+#### test function to run locally
+def test(args, model, device, test_loader):
+    model.eval()
+    test_loss = 0
+    correct = 0
+    with torch.no_grad():
+        for data, target in test_loader:
+            data, target = data.to(device), target.to(device)
+            output = model(data)
+            test_loss += F.nll_loss(output, target, reduction='sum').item() # sum up batch loss
+            pred = output.argmax(1, keepdim=True) # get the index of the max log-probability 
+            correct += pred.eq(target.view_as(pred)).sum().item()
+
+    test_loss /= len(test_loader.dataset)
+
+    print('\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
+        test_loss, correct, len(test_loader.dataset),
+        100. * correct / len(test_loader.dataset)))
+
+#### launch the training
+model = Network().to(device)
+optimizer = optim.SGD(model.parameters(), lr=args.lr) # momentum is not supported at the moment
+
+for epoch in range(1, args.epochs + 1):
+    train(args, model, device, federated_train_loader, optimizer, epoch)
+    test(args, model, device, test_loader)
+
+if (args.save_model):
+    torch.save(model.state_dict(), "mnist_cnn.pt")
